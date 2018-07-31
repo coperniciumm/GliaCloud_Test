6e5ae4d1aa218285902a676041be08445f399037
coperniciumm <joe2582038@gmail.com>
2018-07-31 19:01:43 +0800

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -1,17 +1,17 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-
-base_url = "https://www.ptt.cc"
-home_url = base_url + "/bbs/Lifeismoney/index.html"
+base_url = "https://www.ptt.cc/"
+home_url = "https://www.ptt.cc/bbs/Lifeismoney/index.html"
 res = requests.get(home_url)
 soup = BeautifulSoup(res.text, 'html.parser')
 
 for news in soup.find_all(class_='r-ent'):
-    story_title = news.find('div', attrs={'class': 'title'}).text
-    #story_path = news.find_all('div', attrs={'class': 'title'}).find('a')['href']
-    #story_path = news.find('div', attrs={'class': 'title'}).find(href=re.compile("^/bbs\d+"))
-    story_date = news.find('div', attrs={'class': 'date'}).text
+    date = news.find('div', attrs={'class': 'date'}).text
+    author = news.find('div', attrs={'class': 'author'}).text
+    if news.find('a'):
+        title = news.find('div', attrs={'class': 'title'}).text
+        path = news.find('a')['href']
 
-    print(story_title, story_date)
-    #print(news.text)
+
+    print(title, date, path, author)
